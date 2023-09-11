946054a270865eb265551dbe151ff027a1728186
bjyxszd9595 <1979510475@qq.com>
2023-09-11 19:15:41 -0400

    Update layout.py

---
+++ Lab 2/layout.py
@@ -0,0 +1,122 @@
+import time
+import subprocess
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+import textwrap
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+
+def editImage(filename):
+    image = Image.open(filename)
+    # Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+    return image
+
+def back1():
+    disp.image(editImage('moonland.jpeg'))
+    # Define the "date" text
+    date_text = "1983/1/1"
+
+    # Center and wrap the "yes" text at the top of the screen
+    text = textwrap.fill(date_text, width=10) 
+    text_width, text_height = draw.textsize(text, font)
+    x1 = (width - text_width) // 2
+    y1 = top  
+    draw.text((x1, y1), text, fill=255, font=font)
+
+    # Define the content text
+    content_text = "THE INVENTION OF NETWORK"
+
+    # Center and wrap the "no" text below "yes"
+    text = textwrap.fill(content_text, width=10)  
+    text_width, text_height = draw.textsize(text, font)
+    x2 = (width - text_width) // 2
+    y2 += text_height 
+    draw.text((x2, y2), text, fill=255, font=font)
+
+    # Define the "continue" text
+    continue_text = "> Continue"
+    text = textwrap.fill(content_text, width=10)  
+    text_width, text_height = draw.textsize(text, font)
+    x3 = width*0.1
+    y3 = height*0.7
+    draw.text((x3, y3), text, fill=255, font=font)
+   
+    
+
+while True:
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=400)
+    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+   
+    back1()
+    disp.image(image, rotation)
+    time.sleep(1)
+
