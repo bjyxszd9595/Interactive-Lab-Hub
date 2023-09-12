69e8cc6f95ed234fdb8bc33a0a76ae98a03a8170
bjyxszd9595 <1979510475@qq.com>
2023-09-11 22:30:50 -0400

    Update layout.py

--- Lab 2/layout.py
+++ Lab 2/layout.py
@@ -64,21 +64,20 @@ backlight.value = True
 def editImage(filename):
     image = Image.open(filename).convert("RGB")
     
-    # Scale the image to the smaller screen dimension
-    image_ratio = image.width / image.height
-    screen_ratio = width / height
-    if screen_ratio < image_ratio:
-        scaled_width = image.width * height // image.height
-        scaled_height = height
-    else:
-        scaled_width = width
-        scaled_height = image.height * width // image.width
-    image = image.resize((scaled_width/2, scaled_height/2), Image.BICUBIC)
+   # Calculate the new width (half of the current width) while maintaining the aspect ratio
+    new_width = image.width // 2
+    aspect_ratio = float(image.width) / image.height
+    new_height = int(new_width / aspect_ratio)
+    
+    # Resize the image to half of its current size
+    image = image.resize((new_width, new_height), Image.BICUBIC)
 
-    # Crop and center the image
-    x = scaled_width // 2 - width // 2
-    y = scaled_height // 2 - height // 2
+    # Crop and center the image as needed
+    x = new_width // 2 - width // 2
+    y = new_height // 2 - height // 2
     image = image.crop((x, y, x + width, y + height))
+
+    return image
 
 
 
